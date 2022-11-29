50d9953512e883b4d3d9217c94d17ec5ba2b442a
Luke Jennings <lukejennings123@gmail.com>
2022-11-29 15:43:35 +0000

    connected mongoDB database and created data schema structure

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express') // import express framework
+const mongoose = require('mongoose')
 
 const app = express(); // initalise express server. 
-app.set('view engine', 'ejs'); // initialise ejs as the server view engine. 
+app.set('view engine', 'ejs'); // initialise ejs as the server view engine.
 
-app.listen(3000); // listen to requests on port 3000 (local host) 
+app.use(express.static('/Users/mac1/Node.JS/Inventory-Management-IMaaS-App/public')); // use static files from public folder (css)
 
-app.get('/', (req, res) => {
+
+// app.listen(3000); // listen to requests on port 3000 (local host) 
+
+
+//connect to MongoDB database
+const DBURI = 'mongodb+srv://admin:password12345@projectcluster.pdzilih.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(DBURI).then(() => {
+    console.log('dataBase connected')
+    app.listen(3000); // connect to dataBase then listen for requests. 
+}).catch((error) => { // catch any errors
+    console.log(error)
+})
+
+
+
+
+app.get('/', (req, res) => {    
     res.render('index')
 })
 
---
+++ models/inventory.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const inventoryItemSchema = new Schema({ // this is the documnet schema IE the documnet data structure.
+    name: String,
+    description: String, 
+    category: String, 
+    price: Number, 
+    numInStock: Number, 
+    url: String
+})
+
+const InventoryItems = mongoose.model('InventoryItems', inventoryItemSchema);
+
+module.exports = InventoryItems;
